014677934e6ea5dd7aa4955b71b3c17e42aa1d42
Ubuntu <ubuntu@ip-172-31-53-5.ec2.internal>
2020-10-07 06:59:29 +0000

    change infos prod

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackaton-fiap-20cld-335789"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
